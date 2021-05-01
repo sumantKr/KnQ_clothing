14b377635db471ed23776a04c01ceb74ebc99717
sumant kumar <sumantkumar172000@gmail.com>
2021-05-01 20:12:10 +0530

    homepage

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.scss';
+import { HomePage } from './components/HomePage/Homepage.component.jsx'
+function App() {
+  return (
+    <div className="App">
+    <HomePage/>
+    </div>
+  );
+}
+export default App;
