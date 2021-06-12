8e74637f637a84197c56ba0045f3188889a9fbbd
sumant kumar <sumantkumar172000@gmail.com>
2021-06-12 13:48:53 +0530

    removed logs 2

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -6,7 +6,6 @@ import CollectionPage from '../collection/collection.component'
 import './shop-page.scss'
 const ShopPage = ({ match }) => (
     <div className="shop-page">
-    // {console.log(match)} 
         <Route exact path={match.path} component={CollectionsOverview} />
         <Route exact path={`${match.path}/:collectionId`} component={CollectionPage}/>
         </div>
